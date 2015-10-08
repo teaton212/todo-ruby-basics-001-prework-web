e64c4712786fbeeaf512ab65dd771d5ea267b6dc
teaton212 <tjeaton@gmail.com>
2015-10-08 13:38:28 -0400

    Ruby Basics Successful

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,39 @@
 def division(num1, num2)
- 
+  return num1/num2
 end
+division(36,6)
 
-def assign_variable(value)
-  
+def assign_variable(variable)
+  return variable
 end
+assign_variable("Bob")
 
-def argue
-  
+def argue(phrase)
+  return phrase
 end
+argue("I'm right and you are wrong")
 
-def greeting
-
+def greeting(greeting,name)
+  return greeting + name  
 end
+greeting("hello", "Bob")
 
 def return_a_value
-
+  return "Nice"
 end
+return_a_value
 
 def last_evaluated_value
+  "What do you call"
+  "A person who knows everything?"
+  return "expert"
+  last_evaluated_value
 
 end
 
-def pizza_party
-
+def pizza_party(pizza_party = "cheese")
+  return pizza_party
 end
+pizza_party
+pizza_party("pepperoni")
+
